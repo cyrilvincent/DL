34499c595acbb69a04f57758d5f17ea15ad9d4e9
cyrilvincent <contact@cyrilvincent.com>
2020-06-24 15:21:17 +0200

    ST 0.75/3

---
+++ DL09-06-cancer-overfitting.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer() # more info : https://goo.gl/U2Uwz2
+X=cancer['data']
+y=cancer['target']
+
+import tensorflow as tf
+import tensorflow.keras as keras
+model = keras.Sequential([
+    keras.layers.Dense(30, activation=tf.nn.relu,
+                       input_shape=(X.shape[1],)),
+    keras.layers.Dense(30, activation=tf.nn.relu),
+    keras.layers.Dense(30, activation=tf.nn.relu),
+    keras.layers.Dense(1)
+  ])
+
+model.compile(loss="mse", optimizer="rmsprop",metrics=['accuracy'])
+model.summary()
+
+hist = model.fit(X, y, epochs=200, batch_size=10)
+eval = model.evaluate(X, y)
+print(eval)
+
+import matplotlib.pyplot as plt
+f, ax = plt.subplots()
+ax.plot([None] + hist.history['accuracy'], 'o-')
+ax.legend(['Train accuracy'], loc = 0)
+ax.set_title('Accuracy per Epoch')
+ax.set_xlabel('Epoch')
+ax.set_ylabel('Accuracy')
+plt.show()
+
