188854cfa9bf6e5da862d12be84a6a355b82e1dd
cyrilvincent <contact@cyrilvincent.com>
2020-06-25 10:20:05 +0200

    ST 1.25/3

---
+++ DL09-10-cancer-sigmoid.py
@@ -0,0 +1,28 @@
+import sklearn.datasets
+import sklearn.preprocessing
+
+cancer = sklearn.datasets.load_breast_cancer() # more info : https://goo.gl/U2Uwz2
+X=cancer['data']
+y=cancer['target']
+
+scaler = sklearn.preprocessing.RobustScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+
+import tensorflow as tf
+import tensorflow.keras as keras
+model = keras.Sequential([
+    keras.layers.Dense(30, activation=tf.nn.relu,
+                       input_shape=(X.shape[1],)),
+    keras.layers.Dense(30, activation=tf.nn.relu),
+    keras.layers.Dense(30, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+  ])
+
+model.compile(loss="mse",metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X, y, epochs=200, batch_size=10, validation_split=0.2)
+eval = model.evaluate(X, y)
+print(eval)
+
