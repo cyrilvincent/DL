d0be3560fc38f6a20a0480e00f13f38f159ff040
cyrilvincent <contact@cyrilvincent.com>
2020-11-30 08:51:27 +0100

    GEA

--- DL16-01-tflite-convert.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.models.load_model("data/h5/cholletmodel-mnist.h5")
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-open("data/h5/cholletmodel-mnist.tflite", "wb").write(tflite_model)
