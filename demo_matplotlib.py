c68b6eeba282462991e6077c4200c58dec5364e4
cyrilvincent <contact@cyrilvincent.com>
2020-11-30 14:15:40 +0100

    GEA

--- demo_matplotlib.py
+++ demo_matplotlib.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0,2*np.pi,0.01)
+x = np.arange(-2*np.pi,2*np.pi,0.01)
 y = np.sin(x)
 y2 = np.cos(x)
+y3 = np.tanh(x)
 
 plt.plot(x,y) #scatter, bar
 plt.plot(x,y2) #scatter, bar
+plt.plot(x,y3) #scatter, bar
 plt.show()
 
