3d3352650da1cfd03fe92bf3760dbb0e711d005c
cyrilvincent <contact@cyrilvincent.com>
2020-12-01 16:35:51 +0100

    GEA

--- DL09-24-tp-drivers-10-correction.py
+++ DL09-24-tp-drivers-10-correction.py
@@ -1,22 +1,21 @@
 import pandas as pd
+import tensorflow.keras as keras
 
 print("Load CSV")
-df = pd.read_csv("data/state-farm-distracted-driver-detection/vgg16-bottleneck-train.small.csv")
+df = pd.read_csv("data/state-farm-distracted-driver-detection/vgg16-bottleneck-train.large.csv")
 print(df.head())
 x = df.iloc[:,2:]
 y = df.iloc[:,1]
-x=x[y < 2]
-y=y[y < 2]
+y = keras.utils.to_categorical(y)
 print(y)
 
-import tensorflow.keras as keras
 model = keras.Sequential()
 model.add(keras.layers.Dense(512, input_shape=(x.shape[1],)))
 model.add(keras.layers.Activation('relu'))
-model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dropout(0.1))
 model.add(keras.layers.Dense(256))
 model.add(keras.layers.Activation('relu'))
-model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dropout(0.1))
 model.add(keras.layers.Dense(10))
 model.add(keras.layers.Activation('softmax'))
 
@@ -24,5 +23,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.summary()
 
-history = model.fit(x, y, epochs=50, batch_size=10, validation_split=0.2)
+history = model.fit(x, y, epochs=50, batch_size=1, validation_split=0.2)
 print(model.evaluate(x,y))
+
+model.save("data/h5/drivers_10_mlp.h5")
---
+++ DL09-24-tp-drivers-2-correction.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import tensorflow.keras as keras
+
+print("Load CSV")
+df = pd.read_csv("data/state-farm-distracted-driver-detection/vgg16-bottleneck-train.large.csv")
+print(df.head())
+x = df.iloc[:,2:]
+y = df.iloc[:,1]
+x=x[y < 2]
+y=y[y < 2]
+print(y)
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(512, input_shape=(x.shape[1],)))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(256))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(1))
+model.add(keras.layers.Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(x, y, epochs=500, batch_size=10, validation_split=0.2)
+print(model.evaluate(x,y))
+
+model.save("data/h5/drivers_mlp.h5")
