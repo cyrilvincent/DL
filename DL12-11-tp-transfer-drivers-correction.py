a9339876145b6743dc8f4e56af69e3d898cd2022
cyrilvincent <contact@cyrilvincent.com>
2020-12-02 13:19:30 +0100

    GEA

---
+++ DL12-11-tp-transfer-drivers-correction.py
@@ -0,0 +1,59 @@
+import tensorflow.keras as keras
+
+model = keras.applications.vgg16.VGG16(include_top=False, weights="imagenet", input_shape=(224, 224, 3))
+
+for layer in model.layers:
+    layer.trainable = False
+
+x = model.output
+x = keras.layers.Flatten()(x)
+x = keras.layers.Dense(256, activation="relu")(x)
+x = keras.layers.Dropout(0.2)(x)
+x = keras.layers.Dense(128, activation="relu")(x)
+x = keras.layers.Dropout(0.2)(x)
+x =  keras.layers.Dense(30, activation="relu")(x)
+x = keras.layers.Dropout(0.2)(x)
+x = keras.layers.Dense(10, activation="softmax")(x)
+
+model = keras.models.Model(inputs=model.input, outputs=x)
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+trainset = keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255, validation_split=0.2,
+    shear_range=0.2,
+    zoom_range=0.2)
+
+batchSize = 16
+
+trainGenerator = trainset.flow_from_directory(
+        'data/state-farm-distracted-driver-detection/train-224',
+        target_size=(224, 224),
+        subset = 'training',
+        class_mode="categorical",
+        batch_size=batchSize)
+
+validationGenerator = trainset.flow_from_directory(
+        'data/state-farm-distracted-driver-detection/train-224',
+        target_size=(224, 224),
+        class_mode="categorical",
+        subset = 'validation',
+        batch_size=batchSize)
+
+# checkpointer = keras.callbacks.ModelCheckpoint(filepath = 'data/h5/vgg16-drivers-ckpt-{epoch:03d}-{accuracy:.3f}.h5',
+#                                                          monitor='accuracy'
+#                                                         )
+model.fit(
+        trainGenerator,
+        epochs=30,
+        validation_data=validationGenerator,
+        # callbacks=[checkpointer]
+)
+
+model.save('data/state-farm-distracted-driver-detection/vgg16model.h5')
+
+
+
+
