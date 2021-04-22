5850293b0f874bf1a37d087ceb344f3c76956a31
cyrilvincent <contact@cyrilvincent.com>
2021-04-22 21:55:42 +0200

    Cyril

--- data/climate/jena_timeseries.py
+++ data/climate/jena_timeseries.py
@@ -7,14 +7,135 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import tensorflow as tf
+import data.climate.window_generator as wg
 
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = False
 
-df = pd.read_csv("jena_climate_2009_2016.csv")
+df = pd.read_csv("jena_climate_2009_2016_hourly.csv")
 print(df)
 # TS 10 min )> 1 hour
-df = df[5::6]
-df.to_csv("jena_climate_2009_2016_hourly.csv")
+# df = df[5::6]
+# Replace -9999 => 0
+# wv = df['wv (m/s)']
+# bad_wv = wv == -9999.0
+# wv[bad_wv] = 0.0
+# max_wv = df['max. wv (m/s)']
+# bad_max_wv = max_wv == -9999.0
+# max_wv[bad_max_wv] = 0.0
+# df.to_csv("jena_climate_2009_2016_hourly.csv")
+
 date_time = pd.to_datetime(df.pop('Date Time'), format='%d.%m.%Y %H:%M:%S')
 print(df.describe())
+
+plot_cols = ['T (degC)', 'p (mbar)', 'rho (g/m**3)']
+plot_features = df[plot_cols]
+plot_features.index = date_time
+# Pandas internally call plt
+_ = plot_features.plot(subplots=True)
+plt.show()
+
+plot_features = df[plot_cols][:480]
+plot_features.index = date_time[:480]
+_ = plot_features.plot(subplots=True)
+plt.show()
+
+# Convert deg => radian
+wv = df.pop('wv (m/s)')
+max_wv = df.pop('max. wv (m/s)')
+# Convert to radians.
+wd_rad = df.pop('wd (deg)')*np.pi / 180
+# Calculate the wind x and y components.
+df['Wx'] = wv*np.cos(wd_rad)
+df['Wy'] = wv*np.sin(wd_rad)
+# Calculate the max wind x and y components.
+df['max Wx'] = max_wv*np.cos(wd_rad)
+df['max Wy'] = max_wv*np.sin(wd_rad)
+
+plt.hist2d(df['Wx'], df['Wy'], bins=(50, 50), vmax=400)
+plt.colorbar()
+plt.xlabel('Wind X [m/s]')
+plt.ylabel('Wind Y [m/s]')
+ax = plt.gca()
+ax.axis('tight')
+plt.show()
+
+# Le temps comme géré ici n'est pas utile
+# Nous allons le convertir en férequence avec une périodicité de 24h
+# Convert Datatime => s
+timestamp_s = date_time.map(datetime.datetime.timestamp)
+day = 24*60*60
+year = (365.2425)*day
+df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
+df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
+
+# Fourier Transform
+# RFFT = FFT sans calculer la symétrie (valeur conjugées) = FFT sans redondance inutile
+fft = tf.signal.rfft(df['T (degC)'])
+f_per_dataset = np.arange(0, len(fft))
+n_samples_h = len(df['T (degC)'])
+hours_per_year = 24*365.2524
+years_per_dataset = n_samples_h/(hours_per_year)
+f_per_year = f_per_dataset/years_per_dataset
+plt.step(f_per_year, np.abs(fft))
+plt.xscale('log')
+plt.ylim(0, 400000)
+plt.xlim([0.1, max(plt.xlim())])
+plt.xticks([1, 365.2524], labels=['1/Year', '1/day'])
+_ = plt.xlabel('Frequency (log scale)')
+plt.show()
+# Les températures sont en fréquence identiques tous les 1j et les 1 ans
+
+# Création du test et validation set sans mélange de données
+column_indices = {name: i for i, name in enumerate(df.columns)}
+n = len(df)
+train_df = df[0:int(n*0.7)]
+val_df = df[int(n*0.7):int(n*0.9)]
+test_df = df[int(n*0.9):]
+num_features = df.shape[1]
+
+# Normalisation
+train_mean = train_df.mean()
+train_std = train_df.std()
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+# Verif de la normalisation
+df_std = (df - train_mean) / train_std
+df_std = df_std.melt(var_name='Column', value_name='Normalized')
+plt.figure(figsize=(12, 6))
+ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+_ = ax.set_xticklabels(df.keys(), rotation=90)
+plt.show()
+
+# Fenetrage
+# Regroupement des données avec un pas
+w1 = wg.WindowGenerator(input_width=24, label_width=1, shift=24, train_df=train_df, val_df=val_df, test_df=test_df,
+                     label_columns=['T (degC)'],)
+print(w1)
+w2 = wg.WindowGenerator(input_width=6, label_width=1, shift=1, train_df=train_df, val_df=val_df, test_df=test_df,
+                     label_columns=['T (degC)'])
+print(w2)
+
+example_window = tf.stack([np.array(train_df[:w2.total_window_size]),
+                           np.array(train_df[100:100+w2.total_window_size]),
+                           np.array(train_df[200:200+w2.total_window_size])])
+
+
+example_inputs, example_labels = w2.split_window(example_window)
+
+print('All shapes are: (batch, time, features)')
+print(f'Window shape: {example_window.shape}')
+print(f'Inputs shape: {example_inputs.shape}')
+print(f'labels shape: {example_labels.shape}')
+# Le code ci-dessus a pris un lot de 3 fenêtres à 7 pas de temps, avec 19 fonctionnalités à chaque pas de temps. Il les a divisés en un lot de 6 pas de temps, 19 entrées de fonction et une étiquette à 1 étape de fonction. L'étiquette n'a qu'une seule fonctionnalité car le WindowGenerator été initialisé avec label_columns=['T (degC)']
+
+w2.example = example_inputs, example_labels
+w2.plot()
+plt.show()
+
+w2.plot(plot_col='p (mbar)')
+plt.show()
