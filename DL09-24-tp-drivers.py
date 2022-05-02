6d80eb47f742613ef1cf039d7a2cd8a75fe01f6d
Cyril Vincent <contact@cyrilvincent.com>
2022-05-02 14:03:44 +0200

    ST

--- DL09-24-tp-drivers.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-print("Load CSV")
-df = pd.read_csv("data/state-farm-distracted-driver-detection/vgg16-bottleneck-train.small.csv")
-print(df)
-x = df.iloc[:,2:]
-y = df.iloc[:,1]
-x=x[y < 2]
-y=y[y < 2]
