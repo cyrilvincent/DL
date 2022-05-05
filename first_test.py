a005ad7e121007c58a017ae797fc701ec51b24cb
Cyril Vincent <contact@cyrilvincent.com>
2022-05-05 11:52:07 +0200

    ST

--- first_test.py
+++ first_test.py
@@ -1,8 +1,14 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
+import pandas as pd
+
+# pip install numpy
+# pip install pandas
+# pip install tensorflow
 
 print("Deep Learning")
 print(np.__version__)
+print(pd.__version__)
 print(tf.__version__)
 print(keras.__version__)
