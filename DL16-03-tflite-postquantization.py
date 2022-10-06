8ca18e7f6a54ac38d8b1662a0ab48a739893ecb5
Cyril Vincent <contact@cyrilvincent.com>
2022-10-06 08:33:57 +0200

    CEA

--- DL16-03-tflite-postquantization.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.models.load_model("data/h5/cholletmodel-mnist.h5")
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-converter.optimizations = [tf.lite.Optimize.DEFAULT] # 8 bits par défaut (integer)
-# converter.target_spec.supported_types = [tf.float16] # Passage en 16 bits
-converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE] # Convertit les poids ET les fonctions d'activation en 8 bits
-tflite_model = converter.convert()
-open("data/h5/cholletmodel-mnist-q16.tflite", "wb").write(tflite_model)
