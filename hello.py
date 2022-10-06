9d63cb3664fdb4704d2ae54ff002c49de13da6e0
cyrilvincent <contact@cyrilvincent.Com>
2022-10-06 11:38:02 +0200

    CEA

---
+++ hello.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import sklearn.linear_model as lm
+import matplotlib.pyplot as plt
+
+print("Hello World")
+print(pd.__version__)
+print(np.__version__)
+print(sklearn.__version__)
+
+dataframe = pd.read_csv("data/house/house.csv")
+print(dataframe)
+
+x = dataframe.surface.values.reshape(-1,1)
+y = dataframe.loyer
+
+model = lm.LinearRegression()
+model.fit(x, y)
+
+predicted = model.predict(x)
+
+plt.scatter(x, y)
+plt.plot(x, predicted, color="red")
+plt.show()
+
