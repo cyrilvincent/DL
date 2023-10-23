fc24181be7b8f2d31a2134d1deb0f9b53d7efb75
Cyril Vincent <contact@cyrilvincent.com>
2023-10-23 14:38:58 +0200

    Sparks

---
+++ DL09-01-keras-and.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+tf.random.set_seed(1)
+
+X=np.array([[0,0],[0,1],[1,0],[1,1]])
+y=np.array([0,0,0,1])
+
+import tensorflow.keras as keras
+model = keras.Sequential([
+    keras.layers.Dense(4, input_shape=(X.shape[1],)),
+    keras.layers.Dense(8),
+    keras.layers.Dense(1)
+  ])
+
+model.compile(loss="mse")
+model.summary()
+
+history = model.fit(X, y, epochs=100, batch_size=1)
+
+res = model.predict(X)
+print(res)
+print(res > 0.5)
+
