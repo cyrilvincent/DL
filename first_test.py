29436eb72dd82fef6b751fcf0cfb70cce3ec78dc
Cyril Vincent <contact@cyrilvincent.com>
2023-10-23 10:09:57 +0200

    sparks

--- first_test.py
+++ first_test.py
@@ -1,8 +1,11 @@
 import numpy as np
+import tensorflow as tf
+import sys
 
 # pip install numpy
 # pip install pandas
 # pip install tensorflow
 
 print("Deep Learning")
-print(np.__version__)
+print(sys.version, np.__version__, tf.__version__)
+
