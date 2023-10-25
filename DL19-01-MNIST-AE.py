a59341eb75a4ef8bf421b07a8d6b3f721320f25f
Cyril Vincent <contact@cyrilvincent.com>
2023-10-25 13:23:50 +0200

    Sparks

---
+++ DL19-01-MNIST-AE.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+with np.load("data/mnist/mnist.npz", allow_pickle=True) as f:
+    x_train, y_train = f['x_train'], f['y_train']
+    x_test, y_test = f['x_test'], f['y_test']
+
+x_train = x_train.astype("float32")
+x_test = x_test.astype("float32")
+
+# Normalize value to [0, 1]
+x_train /= 255
+x_test /= 255
+
+latent_dim = 64
+class Autoencoder(tf.keras.Model):
+  def __init__(self, latent_dim):
+    super(Autoencoder, self).__init__()
+    self.latent_dim = latent_dim
+    self.encoder = tf.keras.Sequential([
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(latent_dim, activation='relu'),
+    ])
+    self.decoder = tf.keras.Sequential([
+      tf.keras.layers.Dense(784, activation='sigmoid'),
+      tf.keras.layers.Reshape((28, 28))
+    ])
+
+  def call(self, x):
+    encoded = self.encoder(x)
+    decoded = self.decoder(encoded)
+    return decoded
+
+autoencoder = Autoencoder(latent_dim)
+
+autoencoder.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError())
+
+autoencoder.fit(x_train, x_train,
+                epochs=10,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+encoded_imgs = autoencoder.encoder(x_test).numpy()
+decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+  # display original
+  ax = plt.subplot(2, n, i + 1)
+  plt.imshow(x_test[i])
+  plt.title("original")
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+
+  # display reconstruction
+  ax = plt.subplot(2, n, i + 1 + n)
+  plt.imshow(decoded_imgs[i])
+  plt.title("reconstructed")
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+plt.show()
+
+
